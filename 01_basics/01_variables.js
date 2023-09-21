bceb6423152f161ccb28d69eff4e12bd9baeefff
Yogesh Kumar <123317484+yogeshkumar94s@users.noreply.github.com>
2023-09-21 07:16:24 +0000

    Variables revision

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,5 +1,23 @@
-const myName = "Yogesh kumar"
+/* avaScript is a "dynamically typed language", which means that, unlike some other languages, you don't need to specify what data type a variable will contain (numbers, strings, arrays, etc.).
 
-// myName = "kumar"
+For example, if you declare a variable and give it a value enclosed in quotes, the browser treats the variable as a string:  */
 
-console.log(myName);
+
+
+let myVarianle = "variable"
+
+
+
+/*
+As well as variables, you can declare constants. These are like variables, except that:
+
+you must initialize them when you declare them
+you can't assign them a new value after you've initialized them.
+
+*/
+
+const myConstat = 25;
+
+
+
+//-----------Note---------------- This means that if you can initialize a variable when you declare it, and don't need to reassign it later, make it a constant.
