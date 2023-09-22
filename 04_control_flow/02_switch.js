7d46ca444bff6092cb9b6ada3208d7a08b119617
Yogesh Kumar <123317484+yogeshkumar94s@users.noreply.github.com>
2023-09-22 08:27:51 +0000

    switch statement in javascript

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,64 @@
+// -----------switch -------------
+
+// switch (key) {
+//     case value:
+
+//         break
+
+//     default: 
+//         break
+// }
+
+
+
+// const month = 5; 
+
+// switch (month) {
+//     case 1:
+//         console.log("this is january")
+//         break;
+//     case 2:
+//         console.log("this is february")
+//         break;
+//     case 3:
+//         console.log("this is march")
+//         break;
+//     case 4:
+//         console.log("this is april")
+//         break;
+//     case 5:
+//         console.log("this is may")
+//         break;
+//     case 6:
+//         console.log("this is june")
+//         break;
+//     case 7:
+//         console.log("this is july")
+//         break;
+//     case 8:
+//         console.log("this is august")
+//         break;
+//     case 9:
+//         console.log("this is september")
+//         break;
+//     case 10:
+//         console.log("this is october")
+//         break;
+//     case 11:
+//         console.log("this is november")
+//         break;
+//     case 12:
+//         console.log("this is december")
+//         break;
+
+//     default:
+//         console.log("not a month ")
+
+//         break;
+// }
+
+
+
+
+
+
