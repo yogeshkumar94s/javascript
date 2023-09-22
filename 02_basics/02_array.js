e6baa7ca611be2721eedef5711fed0b714af7284
Yogesh Kumar <123317484+yogeshkumar94s@users.noreply.github.com>
2023-09-22 09:14:45 +0000

    revision

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_hrros)
+
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][1]);
+
+
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+
+// Spread Operators
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros)
+
+
+
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array)
