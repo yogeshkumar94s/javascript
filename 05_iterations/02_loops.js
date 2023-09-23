d51b692da5161479388626974ba401948038e613
Yogesh Kumar <123317484+yogeshkumar94s@users.noreply.github.com>
2023-09-23 05:23:27 +0000

    do while loop

---
+++ 05_iterations/02_loops.js
@@ -0,0 +1,57 @@
+// ---------while loop---------------
+
+
+// let index = 0;
+
+// while (index <= 10) {
+//     console.log(`Value of index is : ${index}`);
+//     index = index + 2;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myArray = ['fles', 'batman', 'superman'];
+
+// let arr = 0;
+
+// while (arr < myArray.length) {
+//     console.log(`Value is : ${myArray[arr]}`);
+//     // arr++;     // ye bhi kr sakte hai
+//     arr = arr + 1;
+// };
+
+
+
+
+
+
+
+
+
+
+// -----------do while loop------------------
+
+
+// let score = 1;              //  pahle print hoga uske baad condition check hogi
+//                             //  agar score = 11 kr diya to -- the score is 11 print ho jayega
+
+// do {
+//     console.log(`The score is : ${score}`);
+//     score++;
+// } while (score <= 10);
+
+
+
+
+
+
