777abb9f525273f4292be5a7304172e5159b67d1
Yogesh Kumar <123317484+yogeshkumar94s@users.noreply.github.com>
2023-09-23 17:38:00 +0000

    for in loop: loop for objects

---
+++ 05_iterations/04_loops.js
@@ -0,0 +1,28 @@
+// ----------------loops for Object---------------
+
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+
+
+// -------------array--------------------
+
+
+const programming = ['js', 'rb', 'py', 'java', 'cpp']
+
+for (const key in programming) {
+//    console.log(key);      // this will print the key of programming array : in this case - 0 1 2 3 4 5 
+      console.log(programming[key]);       // this will print the value of programming array: in this case - js rb py java cpp
+};
+
+
